93f252b98cd1514ac1ef3b6dbddd062bf56284fd
hector <hectorrrivera1010@gmail.com>
2024-09-03 19:06:30 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
   title = 'first-app';
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId)!
+  }
 
   onSelectUser(id: string): void {
-    console.log('selected the user wiht id ' + id);
+    this.selectedUserId = id;
   }
 
 }
