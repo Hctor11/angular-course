1f50f65c5c97a3d04dde5451455d3fc35cf0c699
hector <hectorrrivera1010@gmail.com>
2024-09-08 04:48:48 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [HeaderComponent, UserComponent, TasksComponent] // standalone components
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // standalone components
 })
-
-export class AppModule {
-
-}
+export class AppModule {}
